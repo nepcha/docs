c1159a0f4e22c8d4835443337072a7492b2ac01a
Nepcha Team <hello@nepcha.com>
2023-03-16 17:29:54 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,7 +83,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/nepcha-cover.jpg',
 
       // Disable dark/light toggle
       colorMode: {
